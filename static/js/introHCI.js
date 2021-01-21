f120b2a6e76cabb9fea5c4ae1981eedc7acee3a1
Winston0323 <529841881@qq.com>
2021-01-20 23:02:02 -0800

    finished lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var toggle = 0;
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -10,9 +10,34 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("In the Galaxy FAR FAR AWAY");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
-
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    	if(toggle == 0){
+    		description.html("<p>I'm out</p>");
+    		description.fadeToggle();
+    		toggle = 1;	
+   	 	}else{
+    		description.html("<p>I'm in</p>");
+    		description.fadeToggle();
+    		toggle = 0;
+    	}
+    	
+    }
+    
 }
